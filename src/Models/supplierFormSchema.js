71d1968de69b43f1eba5dd7806024aa06b72164a
Davi Mesquita <davimesquita.2004@gmail.com>
2024-07-19 01:32:00 -0300

    [feat:18] - Criação do Model de fornecedor > > Co-authored-by: @annacbrandao <annacebrandao@gmail.com>
    Signed-off-by: Davi Mesquita <davimesquita.2004@gmail.com>

---
+++ src/Models/supplierFormSchema.js
@@ -0,0 +1,95 @@
+const mongoose = require('mongoose');
+
+const supplierFormSchema = new mongoose.Schema({
+    
+    nome: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    tipoPessoa: {
+        type: String,
+        required: true,
+        enum: ['Jurídica', 'Física']
+    },
+    cpf: {
+        type: Number,
+        required: true,
+        unique: true
+        //immutable: true
+    },
+    status: {
+        type: String,
+        required: true,
+        enum: ['Ativo', 'Inativo']
+    },
+    naturezaTransacao: {
+        type: String,
+        required: true,
+        enum: ['Receita', 'Despesa']
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    nomeContato: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    celular: {
+        type: String,
+        required: true
+    },
+    telefone: {
+        type: String,
+        required: true
+    },
+    cep: {
+        type: Number,
+        required: true
+    },
+    cidade: {
+        type: String,
+        required: true
+    },
+    uf_endereco: {
+        type: String,
+        required: true,
+        enum: ['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO']
+    },
+    logradouro: {
+        type: String,
+        required: true
+    },
+    complemento: {
+        type: String
+    },
+    nomebanco: {
+        type: String,
+        required: true
+    },
+    agencia: {
+        type: string,
+        required: true
+    },
+    numeroBanco: {
+        type: Number,
+        required: true,
+        unique: true
+    },
+    dv: {
+        type: Number,
+        required: true,
+        unique: true
+    },
+    chavePix: {
+        type: String,
+        required: true,
+        unique: true
+    }
+})
+
+const supplierForm = mongoose.model('Supplier', supplierFormSchema);
+module.exports = supplierForm;
