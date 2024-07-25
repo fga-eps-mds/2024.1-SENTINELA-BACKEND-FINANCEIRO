900404f00cce412448f96d9d150e755a20010ef8
Anna Clara Cardoso Evangelista Brandão <annacebrandao@gmail.com>
2024-07-25 01:52:17 -0300

    [fix:18] - Arrumando propriedades dos campos
    Co-authored-by: @davimesquit <davimesquita.2004@gmail.com>
    Signed-off-by: Anna Clara Cardoso Evangelista Brandão <annacebrandao@gmail.com>

--- src/Models/supplierFormSchema.js
+++ src/Models/supplierFormSchema.js
@@ -9,84 +9,70 @@ const supplierFormSchema = new mongoose.Schema({
     },
     tipoPessoa: {
         type: String,
-        required: true,
+        required: false,
         enum: ['Jurídica', 'Física']
     },
     cpfCnpj: {
         type: String,
-        required: true,
         unique: true,
         immutable: true
     },
     statusFornecedor: {
         type: String,
-        required: true,
         enum: ['Ativo', 'Inativo']
     },
     naturezaTransacao: {
         type: String,
-        required: true,
         enum: ['Receita', 'Despesa']
     },
     email: {
         type: String,
-        required: true,
         unique: true
     },
     nomeContato: {
         type: String,
-        required: true,
-        unique: true
     },
     celular: {
         type: String,
-        required: true
+        unique: true
     },
     telefone: {
         type: String,
-        required: true
+        unique: true
     },
     cep: {
         type: Number,
-        required: true
+        unique: true
     },
     cidade: {
         type: String,
-        required: true
     },
     uf_endereco: {
         type: String,
-        required: true,
         enum: ['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO']
     },
     logradouro: {
         type: String,
-        required: true
     },
     complemento: {
         type: String
     },
     nomeBanco: {
         type: String,
-        required: true
     },
     agencia: {
         type: String,
-        required: true
     },
     numeroBanco: {
         type: Number,
-        required: true,
         unique: true
     },
     dv: {
         type: Number,
-        required: true,
         unique: true
     },
     chavePix: {
         type: String,
-        required: true,
         unique: true
     },
     createdAt: {
