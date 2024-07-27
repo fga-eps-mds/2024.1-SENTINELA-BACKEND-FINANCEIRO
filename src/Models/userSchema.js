ebc72eb7f2d9ab7c66238a641ab26d08350271a7
saracampss <saracampxx@gmail.com>
2024-07-27 14:42:04 -0300

    [config] - setup eslint, prettier, workflows ci e pre commit

--- src/Models/userSchema.js
+++ src/Models/userSchema.js
@@ -1,20 +1,20 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const newSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now
-  },
-  updatedAt: {
-    type: Date,
-    default: Date.now
-  },
+    name: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now,
+    },
 });
 
-const NewSchema = mongoose.model('NewSchema', newSchema);
+const NewSchema = mongoose.model("NewSchema", newSchema);
 
 module.exports = NewSchema;
