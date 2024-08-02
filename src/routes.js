752bfd096b481c3eed2895c9f96cb817dc98aa0e
Victor Yukio Cavalcanti Miki <36861357+yukioz@users.noreply.github.com>
2024-08-01 22:53:31 -0300

    Revert "18-supplier-form"

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,13 @@
 const express = require('express');
 const routes = express.Router();
-const supplierFormController = require('./Controllers/supplierFormController');
+const NewController = require('./Controllers/newController');
 
 // Private
 // routes.get('/finance', tokenValidation, ???.getUsers);
 // routes.get('/finance/:id', tokenValidation,  ???.getUserById);
-routes.post('/SupplierForm/create', supplierFormController.createSupplierForm);
-routes.get('/SupplierForm', supplierFormController.getSupplierForm);
-routes.get('/SupplierForm/:id', supplierFormController.getSupplierFormById);
-routes.delete('/SupplierForm/delete/:id', supplierFormController.deleteSupplierFormById);
-routes.patch('/SupplierForm/update/:id', supplierFormController.updateSupplierFormById);
 
 // Public
+routes.post('/finance/create', NewController.createNew);
+routes.get('/finance', NewController.getNews);
 
 module.exports = routes;
