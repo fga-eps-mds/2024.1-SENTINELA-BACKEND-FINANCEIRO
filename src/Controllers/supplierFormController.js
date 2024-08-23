4de0ec915ca3d6f6326531dd0e2bc62257db2d8a
AlGouvea <alvarohsgouvea@gmail.com>
2024-08-23 17:40:20 -0300

    Revert "Merge pull request #6 from fga-eps-mds/devel"
    This reverts commit 0984c7c96c9415911915d604c07da92a9e9045bd, reversing
    changes made to 37fde139ced183984461ebedbd1e45ba73d08e7b.

--- src/Controllers/supplierFormController.js
+++ src/Controllers/supplierFormController.js
@@ -2,70 +2,68 @@ const SupplierForm = require("../Models/supplierFormSchema");
 const util = require("../Util/utils");
 
 const createSupplierForm = async (req, res) => {
-    try {
-        message = util.validator(req.body.supplierData);
+  try {
+    message = util.validator(req.body.supplierData);
 
-        if (message) {
-            return res.status(404).send({ erro: message });
-        }
-
-        const supplier = new SupplierForm(req.body.supplierData);
-
-        await supplier.save();
-        return res.status(201).send(supplier);
-    } catch (error) {
-        return res.status(400).send(error);
+    if (message) {
+      return res.status(404).send({ erro: message });
     }
+
+    const supplier = new SupplierForm(req.body.supplierData);
+
+    await supplier.save();
+    return res.status(201).send(supplier);
+  } catch (error) {
+    return res.status(400).send(error);
+  }
 };
 
 const getSupplierForm = async (req, res) => {
-    try {
-        const supplier = await SupplierForm.find({});
-        return res.status(200).send(supplier);
-    } catch (error) {
-        return res.status(400).send(error);
-    }
+  try {
+    const supplier = await SupplierForm.find({});
+    return res.status(200).send(supplier);
+  } catch (error) {
+    return res.status(400).send(error);
+  }
 };
 
 const getSupplierFormById = async (req, res) => {
-    try {
-        const supplier = await SupplierForm.findById(req.params.id);
-        return res.status(200).send(supplier);
-    } catch (error) {
-        return res.status(400).send(error);
-    }
+  try {
+    const supplier = await SupplierForm.findById(req.params.id);
+    return res.status(200).send(supplier);
+  } catch (error) {
+    return res.status(400).send(error);
+  }
 };
 
 const deleteSupplierFormById = async (req, res) => {
-    try {
-        const deletedSupplier = await SupplierForm.findByIdAndDelete(
-            req.params.id
-        );
-        return res.status(200).send(deletedSupplier);
-    } catch (error) {
-        return res.status(400).send(error);
-    }
+  try {
+    const deletedSupplier = await SupplierForm.findByIdAndDelete(req.params.id);
+    return res.status(200).send(deletedSupplier);
+  } catch (error) {
+    return res.status(400).send(error);
+  }
 };
 
 const updateSupplierFormById = async (req, res) => {
-    try {
-        const updatedSupplier = await SupplierForm.findById(req.params.id);
-        if (!updatedSupplier) {
-            return res.status(404).send();
-        }
-        Object.assign(updatedSupplier, req.body.supplierData);
-        updatedSupplier.updatedAt = new Date();
-        await updatedSupplier.save();
-        return res.status(200).send(updatedSupplier);
-    } catch (error) {
-        return res.status(400).send(error);
+  try {
+    const updatedSupplier = await SupplierForm.findById(req.params.id);
+    if (!updatedSupplier) {
+      return res.status(404).send();
     }
+    Object.assign(updatedSupplier, req.body.supplierData);
+    updatedSupplier.updatedAt = new Date();
+    await updatedSupplier.save();
+    return res.status(200).send(updatedSupplier);
+  } catch (error) {
+    return res.status(400).send(error);
+  }
 };
 
 module.exports = {
-    createSupplierForm,
-    getSupplierForm,
-    getSupplierFormById,
-    deleteSupplierFormById,
-    updateSupplierFormById,
+  createSupplierForm,
+  getSupplierForm,
+  getSupplierFormById,
+  deleteSupplierFormById,
+  updateSupplierFormById,
 };
--- src/Models/supplierFormSchema.js
+++ src/Models/supplierFormSchema.js
@@ -1,124 +1,96 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const supplierFormSchema = new mongoose.Schema({
+    
     nome: {
         type: String,
         required: true,
-        unique: true,
+        unique: true
     },
     tipoPessoa: {
         type: String,
-        enum: ["Jurídica", "Física", ""],
+        enum: ['Jurídica', 'Física', '']
     },
     cpfCnpj: {
         type: String,
         unique: true,
         immutable: true,
-        sparse: true,
+        sparse: true
     },
     statusFornecedor: {
         type: String,
-        enum: ["Ativo", "Inativo", ""],
+        enum: ['Ativo', 'Inativo', '']
     },
     naturezaTransacao: {
         type: String,
-        enum: ["Receita", "Despesa", ""],
+        enum: ['Receita', 'Despesa', '']
     },
     email: {
         type: String,
         unique: true,
-        sparse: true,
+        sparse:true
     },
     nomeContato: {
-        type: String,
+        type: String
     },
     celular: {
         type: String,
         unique: true,
-        sparse: true,
+        sparse:true
     },
     telefone: {
         type: String,
         unique: true,
-        sparse: true,
+        sparse:true
     },
     cep: {
         type: String,
         unique: true,
-        sparse: true,
+        sparse:true
     },
     cidade: {
-        type: String,
+        type: String
     },
     uf_endereco: {
         type: String,
-        enum: [
-            "AC",
-            "AL",
-            "AP",
-            "AM",
-            "BA",
-            "CE",
-            "DF",
-            "ES",
-            "GO",
-            "MA",
-            "MT",
-            "MS",
-            "MG",
-            "PA",
-            "PB",
-            "PR",
-            "PE",
-            "PI",
-            "RJ",
-            "RN",
-            "RS",
-            "RO",
-            "RR",
-            "SC",
-            "SP",
-            "SE",
-            "TO",
-            "",
-        ],
+        enum: ['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO', '']
     },
     logradouro: {
-        type: String,
+        type: String
     },
     complemento: {
-        type: String,
+        type: String
     },
     nomeBanco: {
-        type: String,
+        type: String
     },
     agencia: {
-        type: String,
+        type: String
     },
     numeroBanco: {
         type: String,
         unique: true,
-        sparse: true,
+        sparse: true
     },
     dv: {
         type: String,
         unique: true,
-        sparse: true,
+        sparse: true
     },
     chavePix: {
         type: String,
         unique: true,
-        sparse: true,
+        sparse: true
     },
     createdAt: {
         type: Date,
-        default: Date.now,
+        default: Date.now
     },
     updatedAt: {
         type: Date,
-        default: Date.now,
+        default: Date.now
     },
-});
+})
 
-const supplierForm = mongoose.model("Supplier", supplierFormSchema);
+const supplierForm = mongoose.model('Supplier', supplierFormSchema);
 module.exports = supplierForm;
