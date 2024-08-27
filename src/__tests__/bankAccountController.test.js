7509e9b0542eebaeeb0d48e7d6e67de363f161e3
Milena Linda <milenafernandes594@gmail.com>
2024-08-26 23:28:26 -0300

    [test-20] - test of bankAccountController
    Co-Authored-by: Diogo-Barboza <diogorodriguesbb@gmail.com>
    Signed-off-by: Milena Linda <milenafernandes594@gmail.com>

--- src/__tests__/bankAccountController.test.js
+++ src/__tests__/bankAccountController.test.js
@@ -73,13 +73,21 @@ describe("BankAccount API", () => {
         expect(response.body.error).toBe("Nome já cadastrado");
     });
 
-    it("should fetch all bank accounts", async () => {
-        const response = await request(app).get("/finance/getBankAccount"); // Atualize o caminho da rota
+    it("should not create a bank account with a blank name", async () => {
+        const response = await request(app)
+            .post("/finance/createBankAccount") // Atualize o caminho da rota
+            .send({
+                formData: {
+                    name: "",
+                    bank: "Banco Teste",
+                    accountNumber: "12345678",
+                    status: "Ativo",
+                    accountType: "Conta Corrente",
+                },
+            });
 
-        console.log("Fetch All Response:", response.body); // Adicione um log para depuração
-
-        expect(response.status).toBe(200);
-        expect(Array.isArray(response.body)).toBe(true);
+        expect(response.status).toBe(400);
+        expect(response.body.error).toBe("Nome não fornecido");
     });
 
     it("should fetch a bank account by ID", async () => {
@@ -105,6 +113,58 @@ describe("BankAccount API", () => {
         expect(response.body.name).toBe("Conta Teste ID");
     });
 
+    it("should not fetch a bank account without ID", async () => {
+        await request(app)
+            .post("/finance/createBankAccount") // Atualize o caminho da rota
+            .send({
+                formData: {
+                    name: "Conta Teste ID",
+                    bank: "Banco Teste ID",
+                    accountNumber: "11111111",
+                    status: "Ativo",
+                    accountType: "Conta Corrente",
+                },
+            });
+
+        const response = await request(app).get(`/finance/bankAccount/${null}`); // Atualize o caminho da rota
+
+        console.log("Fetch By ID Response:", response.body); // Adicione um log para depuração
+
+        expect(response.status).toBe(500);
+    });
+    it("should fetch all bank accounts", async () => {
+        await request(app)
+            .post("/finance/createBankAccount") // Atualize o caminho da rota
+            .send({
+                formData: {
+                    name: "Conta Teste 1",
+                    bank: "Banco Teste 1",
+                    accountNumber: "11111111",
+                    status: "Ativo",
+                    accountType: "Conta Corrente",
+                },
+            });
+
+        await request(app)
+            .post("/finance/createBankAccount") // Atualize o caminho da rota
+            .send({
+                formData: {
+                    name: "Conta Teste 2",
+                    bank: "Banco Teste 2",
+                    accountNumber: "22222222",
+                    status: "Ativo",
+                    accountType: "Conta Corrente",
+                },
+            });
+
+        const response = await request(app).get("/finance/getBankAccount"); // Atualize o caminho da rota
+
+        console.log("Fetch All Response:", response.body); // Adicione um log para depuração
+
+        expect(response.status).toBe(200);
+        expect(response.body.length).toBe(2);
+    });
+
     it("should update a bank account", async () => {
         const newAccount = await request(app)
             .post("/finance/createBankAccount") // Atualize o caminho da rota
@@ -128,6 +188,28 @@ describe("BankAccount API", () => {
         expect(response.body.name).toBe("Conta Atualizada");
     });
 
+    it("should not update a bank account without id", async () => {
+        await request(app)
+            .post("/finance/createBankAccount") // Atualize o caminho da rota
+            .send({
+                formData: {
+                    name: "Conta a ser Atualizada",
+                    bank: "Banco Teste Atualização",
+                    accountNumber: "22222222",
+                    status: "Ativo",
+                    accountType: "Conta Corrente",
+                },
+            });
+
+        const response = await request(app)
+            .patch(`/finance/updateBankAccount/${null}`) // Atualize o caminho da rota
+            .send({ name: "Conta Atualizada" });
+
+        console.log("Update Response:", response.body); // Adicione um log para depuração
+
+        expect(response.status).toBe(400);
+    });
+
     it("should delete a bank account", async () => {
         const newAccount = await request(app)
             .post("/finance/createBankAccount") // Atualize o caminho da rota
@@ -149,5 +231,22 @@ describe("BankAccount API", () => {
 
         expect(response.status).toBe(200);
         expect(response.body.message).toBe("Conta deletada com sucesso");
+    });
+    it("should delete a bank account", async () => {
+        const response = await request(app).delete(
+            `/finance/deleteBankAccount/${null}`
+        ); // Atualize o caminho da rota
+
+        expect(response.status).toBe(500);
+    });
+
+    it("should return 404 if bank account is not found by ID", async () => {
+        const invalidId = new mongoose.Types.ObjectId(); // Gerar um ID válido, mas que não está no banco
+
+        const response = await request(app).get(
+            `/finance/getBankAccountbyId/${invalidId}`
+        );
+
+        expect(response.status).toBe(404);
     });
 });
