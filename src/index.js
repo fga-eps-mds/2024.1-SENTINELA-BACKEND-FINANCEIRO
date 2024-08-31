a530e23d6b5ebe11c92f5395518c5b09c1063742
Jagaima <davinobre.ik@gmail.com>
2024-08-31 12:26:26 -0300

    [fix:36] - reforcing project
    Co-authored-by: Jose Eduardo <jevrprado@gmail.com>
    Signed-off-by: Jagaima <davinobre.ik@gmail.com>

--- src/index.js
+++ src/index.js
@@ -17,7 +17,19 @@ console.log("MONGO_URI:", MONGO_URI);
 console.log("OFFICIAL_MONGO_URI:", OFFICIAL_MONGO_URI);
 
 // Middleware
-app.use(cors());
+const allowedOrigins = ["http://localhost:5173"];
+app.use(
+    cors({
+        origin: function (origin, callback) {
+            if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+                callback(null, true);
+            } else {
+                callback(new Error("Not allowed by CORS"));
+            }
+        },
+    })
+);
+
 app.use(bodyParser.json());
 
 const getFinancialMovementsFromDatabase = async () => {
