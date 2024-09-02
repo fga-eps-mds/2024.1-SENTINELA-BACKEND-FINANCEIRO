059e3dba551f46078e2615186b84dabd9f9625df
Jose Eduardo <jevrprado@gmail.com>
2024-09-02 09:32:30 -0300

    [fix:36] - deploy error
    Signed-off-by: Jose Eduardo <jevrprado@gmail.com>

--- src/index.js
+++ src/index.js
@@ -22,17 +22,7 @@ const allowedOrigins = [
     "https://devel--appsentinela.netlify.app",
     "https://appsentinela.netlify.app",
 ];
-app.use(
-    cors({
-        origin: function (origin, callback) {
-            if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-                callback(null, true);
-            } else {
-                callback(new Error("Not allowed by CORS"));
-            }
-        },
-    })
-);
+app.use(cors());
 
 app.use(bodyParser.json());
 
