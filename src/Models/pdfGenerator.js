f96199cf6a798a4233c57cd511227eb7b8396f12
Jose Eduardo <jevrprado@gmail.com>
2024-09-03 16:24:20 -0300

    [fix:36] - filter pdf
    Co-authored-by: Jagaima <davinobre.ik@gmail.com>
    Signed-off-by: Jose Eduardo <jevrprado@gmail.com>

--- src/Models/pdfGenerator.js
+++ src/Models/pdfGenerator.js
@@ -6,13 +6,17 @@ const formatDate = (date) => {
     const options = {
         weekday: "long",
         year: "numeric",
-        month: "long",
+        month: "numeric",
         day: "numeric",
     };
     return new Date(date).toLocaleDateString("pt-BR", options);
 };
 
-const generateFinancialReportPDF = (financialMovements, filePath) => {
+const generateFinancialReportPDF = (
+    financialMovements,
+    filePath,
+    includeFields
+) => {
     return new Promise((resolve, reject) => {
         const doc = new PDFDocument({ margin: 30 });
         const stream = fs.createWriteStream(filePath);
@@ -27,24 +31,49 @@ const generateFinancialReportPDF = (financialMovements, filePath) => {
                 .text(`Movimento #${index + 1}`, { underline: true })
                 .moveDown();
 
-            doc.text(`Conta Origem: ${movement.contaOrigem}`);
-            doc.text(`Conta Destino: ${movement.contaDestino}`);
-            doc.text(`Nome Origem: ${movement.nomeOrigem}`);
-            doc.text(`Nome Destino: ${movement.nomeDestino}`);
-            doc.text(`Tipo Documento: ${movement.tipoDocumento}`);
-            doc.text(`Valor Bruto: ${movement.valorBruto}`);
-            doc.text(`Valor Líquido: ${movement.valorLiquido || "N/A"}`);
-            doc.text(`Forma de Pagamento: ${movement.formadePagamento}`);
-            doc.text(
-                `Data de Vencimento: ${formatDate(movement.datadeVencimento)}`
-            );
-            doc.text(
-                `Data de Pagamento: ${formatDate(movement.datadePagamento)}`
-            );
-            doc.text(
-                `Situação de Pagamento: ${movement.baixada ? "Pago" : "Não pago"}`
-            );
-            doc.text(`Descrição: ${movement.descricao || "N/A"}`);
+            // Adicione os campos com base no `includeFields`
+            if (includeFields.includes("contaOrigem")) {
+                doc.text(`Conta Origem: ${movement.contaOrigem}`);
+            }
+            if (includeFields.includes("contaDestino")) {
+                doc.text(`Conta Destino: ${movement.contaDestino}`);
+            }
+            if (includeFields.includes("nomeOrigem")) {
+                doc.text(`Nome Origem: ${movement.nomeOrigem}`);
+            }
+            if (includeFields.includes("nomeDestino")) {
+                doc.text(`Nome Destino: ${movement.nomeDestino}`);
+            }
+            if (includeFields.includes("tipoDocumento")) {
+                doc.text(`Tipo Documento: ${movement.tipoDocumento}`);
+            }
+            if (includeFields.includes("valorBruto")) {
+                doc.text(`Valor Bruto: ${movement.valorBruto}`);
+            }
+            if (includeFields.includes("valorLiquido")) {
+                doc.text(`Valor Líquido: ${movement.valorLiquido || "N/A"}`);
+            }
+            if (includeFields.includes("formaPagamento")) {
+                doc.text(`Forma de Pagamento: ${movement.formadePagamento}`);
+            }
+            if (includeFields.includes("dataVencimento")) {
+                doc.text(
+                    `Data de Vencimento: ${formatDate(movement.datadeVencimento)}`
+                );
+            }
+            if (includeFields.includes("dataPagamento")) {
+                doc.text(
+                    `Data de Pagamento: ${formatDate(movement.datadePagamento)}`
+                );
+            }
+            if (includeFields.includes("sitPagamento")) {
+                doc.text(
+                    `Situação de Pagamento: ${movement.baixada ? "Pago" : "Não pago"}`
+                );
+            }
+            if (includeFields.includes("descricao")) {
+                doc.text(`Descrição: ${movement.descricao || "N/A"}`);
+            }
 
             doc.moveDown();
         });
